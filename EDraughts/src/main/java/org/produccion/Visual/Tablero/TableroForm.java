fd5f606367042532cbb5a5a6fb670da66c32f619
GabrielQuiroz25 <gabrielquiroz@uni.pe>
2024-04-07 22:00:58 -0500

    Tablero y fichas con listener... (Probar ficha 9)

--- EDraughts/src/main/java/org/produccion/Visual/Tablero/TableroForm.java
+++ EDraughts/src/main/java/org/produccion/Visual/Tablero/TableroForm.java
@@ -1,8 +1,8 @@
 package org.produccion.Visual.Tablero;
 
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class TableroForm extends JFrame{
     private JPanel panelTablero;
@@ -39,12 +39,242 @@ public class TableroForm extends JFrame{
     private PanelFichas pnlFicha31;
     private PanelFichas pnlFicha32;
     private JButton btnFicha1;
+    private JButton btnFicha2;
+    private JButton btnFicha3;
+    private JButton btnFicha4;
+    private JButton btnFicha5;
+    private JButton btnFicha6;
+    private JButton btnFicha7;
+    private JButton btnFicha8;
+    private JButton btnFicha9;
+    private JButton btnFicha10;
+    private JButton btnFicha11;
+    private JButton btnFicha12;
+    private JButton btnFicha13;
+    private JButton btnFicha14;
+    private JButton btnFicha15;
+    private JButton btnFicha16;
+    private JButton btnFicha17;
+    private JButton btnFicha18;
+    private JButton btnFicha19;
+    private JButton btnFicha20;
+    private JButton btnFicha21;
+    private JButton btnFicha22;
+    private JButton btnFicha23;
+    private JButton btnFicha24;
+    private JButton btnFicha25;
+    private JButton btnFicha26;
+    private JButton btnFicha27;
+    private JButton btnFicha28;
+    private JButton btnFicha29;
+    private JButton btnFicha30;
+    private JButton btnFicha31;
+    private JButton btnFicha32;
+
 
     public TableroForm() {
-        pnlFicha25.addMouseListener(new MouseAdapter() {
+        btnFicha1.addActionListener(new ActionListener() {
             @Override
-            public void mouseClicked(MouseEvent e) {
-                super.mouseClicked(e);
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha7.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha8.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha9.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                if (pnlFicha9.estado == 1){
+                    pnlFicha9.estado = 2;
+                }else if(pnlFicha9.estado == 2){
+                    pnlFicha9.estado = 3;
+                }else if(pnlFicha9.estado == 3){
+                    pnlFicha9.estado = 4;
+                }else if(pnlFicha9.estado == 4){
+                    pnlFicha9.estado = 0;
+                }else if(pnlFicha9.estado == 0){
+                    pnlFicha9.estado = 1;
+                }
+
+
+            }
+        });
+        btnFicha10.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha11.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha12.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha13.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha14.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha15.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha16.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha17.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha18.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha19.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha20.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha21.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha22.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha23.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha24.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha25.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha26.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha27.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha28.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha29.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha30.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha31.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+            }
+        });
+        btnFicha32.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
             }
         });
     }
@@ -52,6 +282,7 @@ public class TableroForm extends JFrame{
     private void createUIComponents() {
         // TODO: place custom component creation code here
         panelTablero = new PanelFondoTablero();
+
         pnlFicha1 = new PanelFichas(1);
         pnlFicha2 = new PanelFichas(1);
         pnlFicha3 = new PanelFichas(1);
