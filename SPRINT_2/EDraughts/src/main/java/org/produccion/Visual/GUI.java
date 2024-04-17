b7ea3f76d90af454bf7614bf710cd713ae540901
Lerzundi <juan.lerzundi.r@uni.pe>
2024-04-17 09:40:55 -0500

    Ejemplo funcional nueva gui

--- SPRINT_2/EDraughts/src/main/java/org/produccion/Visual/GUI.java
+++ SPRINT_2/EDraughts/src/main/java/org/produccion/Visual/GUI.java
@@ -1,5 +1,6 @@
 package org.produccion.Visual;
 
+import java.awt.*;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import javax.swing.JFrame;
@@ -8,37 +9,61 @@ import javax.swing.SwingUtilities;
 
 public class GUI extends JFrame {
 
-  private JPanel panelMain;
-  public GUI() {
-    Lienzo lienzo = new Lienzo();
-    this.setContentPane(lienzo);
-    this.setTitle("E-Draughts");
-    this.setSize(650,648);
-    this.setResizable(false);
-    this.setVisible(true);
-    this.setDefaultCloseOperation(EXIT_ON_CLOSE);
-    //fLogin.dispose();//Cierra solo la ventana
+    private JPanel panelMain;
 
-  }
+    public GUI() {
+        Lienzo lienzo = new Lienzo();
+        this.setContentPane(lienzo);
+        this.setTitle("E-Draughts");
+        this.setSize(640, 640);
+        this.setResizable(false);
+        this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+        this.setVisible(true);
+        //fLogin.dispose();//Cierra solo la ventana
 
-  class Lienzo extends JPanel {
-    Lienzo(){
-      addMouseListener(new MouseAdapter() {
-        public void mouseClicked(MouseEvent e) {
-            int rowSelected = e.getY();
-            int colSelected = e.getX();
-          System.out.println("X: " + colSelected+" Y: "+rowSelected+"\n");
-            //          repaint();
-        }
-      });
     }
-  }
 
-  public static void main(String[] args) {
-    SwingUtilities.invokeLater(new Runnable() {
-      public void run() {
-        new GUI();
-      }
-    });
-  }
+    class Lienzo extends JPanel {
+        Lienzo() {
+
+            addMouseListener(new MouseAdapter() {
+                public void mouseClicked(MouseEvent e) {
+                    int rowSelected = e.getY();
+                    int colSelected = e.getX();
+                    System.out.println("X: " + colSelected + " Y: " + rowSelected + "\n");
+                    //          repaint();
+                }
+            });
+        }
+
+        @Override
+        protected void paintComponent(Graphics g) {
+            super.paintComponent(g);
+            setBackground(Color.YELLOW);
+            drawGridLines(g);
+        }
+
+        private void drawGridLines(Graphics g){
+            g.setColor(Color.BLACK);
+            for (int row = 1; row < 8; ++row) {
+                g.fillRoundRect(0, 80 * row - 4,
+                        640-1, 8, 8, 8);
+            }
+            for (int col = 1; col < 8; ++col) {
+                g.fillRoundRect(80 * col - 4, 0,
+                        8, 640-1, 8, 8);
+            }
+
+        }
+    }
+
+
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                new GUI();
+            }
+        });
+    }
 }
